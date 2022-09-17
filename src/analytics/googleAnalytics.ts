35e3a0692a92a084efa7eed0e424b19ef73b0122
Jonathan Vu <jono.vu@gmail.com>
2022-09-18 09:56:23 +1000

    fix: prefixed env vars with REACT_APP

--- src/analytics/googleAnalytics.ts
+++ src/analytics/googleAnalytics.ts
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga4";
 
 function trackPageView(path: string) {
-  ReactGA.initialize(process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID!);
+  ReactGA.initialize(process.env.REACT_APP_GA_MEASUREMENT_ID!);
   ReactGA.send({ hitType: "pageview", page: path });
 }
 
